fb4c27e51d04c46925685173c48a993e4fbe3efa
tusharpahuja <tushar.pahuja2015@vit.ac.in>
2017-01-30 21:11:33 +0530

    Code

---
+++ Caisa and Pylons.cpp
@@ -0,0 +1,48 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define gc getchar_unlocked
+#define fo(i,n) for(i=0;i<n;i++)
+#define Fo(i,k,n) for(i=k;k<n?i<n:i>n;k<n?i+=1:i-=1)
+#define ll long long
+#define si(x)	scanf("%d",&x)
+#define sl(x)	scanf("%lld",&x)
+#define ss(s)	scanf("%s",s)
+#define pi(x)	printf("%d\n",x)
+#define pl(x)	printf("%lld\n",x)
+#define ps(s)	printf("%s\n",s)
+#define pb push_back
+#define mp make_pair
+#define F first
+#define S second
+#define all(x) x.begin(), x.end()
+#define clr(x) memset(x, 0, sizeof(x))
+#define mav(a) *max_element(a.begin(),a.end())
+#define miv(a) *min_element(a.begin(),a.end())
+#define maa(a,n) *max_element(a,a+n)
+#define mia(a,n) *min_element(a,a+n)
+#define sortall(x) sort(all(x))
+#define tr(it, a) for(auto it = a.begin(); it != a.end(); it++)
+#define PI 3.1415926535897932384626
+#define bye exit(0);
+
+typedef pair<int, int>	pii;
+typedef pair<ll, ll>	pl;
+typedef vector<int>		vi;
+typedef vector<ll>		vl;
+typedef vector<pii>		vpii;
+typedef vector<pl>		vpl;
+typedef vector<vi>		vvi;
+typedef vector<vl>		vvl;
+
+
+int main(){
+
+ll n,i,t=0,val=0;
+cin>>n;
+ll a[n];
+fo(i,n){
+    cin>>a[i];
+}
+cout<<maa(a,n);
+return 0;
+}
