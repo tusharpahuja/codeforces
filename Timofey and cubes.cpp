966714e267b502c3224a705c4f6401a8a0e446e1
tusharpahuja <tushar.pahuja2015@vit.ac.in>
2017-02-03 00:40:34 +0530

    Code

---
+++ Timofey and cubes.cpp
@@ -0,0 +1,65 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define gc getchar_unlocked
+#define fo(i,n) for(i=0;i<n;i++)
+#define Fo(i,k,n) for(i=k;k<n?i<n:i>n;k<n?i+=1:i-=1)
+#define ll long long
+#define si(x)	scanf("%d",&x)
+#define sl(x)	scanf("%lld",&x)
+#define ss(s)	scanf("%s",s)
+#define pi(x)	printf("%d\n",x)
+#define pl(x)	printf("%lld\n",x)
+#define ps(s)	printf("%s\n",s)
+#define pb push_back
+#define mp make_pair
+#define F first
+#define S second
+#define all(x) x.begin(), x.end()
+#define clr(x) memset(x, 0, sizeof(x))
+#define sortall(x) sort(all(x))
+#define tr(it, a) for(auto it = a.begin(); it != a.end(); it++)
+#define PI 3.1415926535897932384626
+#define ret return 0;
+#define bye exit(0);
+typedef pair<int, int>	pii;
+typedef pair<ll, ll>	pl;
+typedef vector<int>		vi;
+typedef vector<ll>		vl;
+typedef vector<pii>		vpii;
+typedef vector<pl>		vpl;
+typedef vector<vi>		vvi;
+typedef vector<vl>		vvl;
+ll GCD(ll a,ll b){
+    if(b==0){
+        return a;
+    }
+    else{
+        return GCD(b,a%b);
+    }
+}
+
+int main(){
+ll n,i,j,c=1,t;
+cin>>n;
+ll a[n];
+fo(i,n){
+    cin>>a[i];
+}
+for(i=0;i<n/2;i++){
+    if(c%2!=0){
+        t = a[i];
+        a[i] = a[n-i-1];
+        a[n-i-1] = t;
+    }
+    c++;
+}
+fo(i,n){
+    cout<<a[i]<<" ";
+}
+
+
+
+
+
+ret;
+}
